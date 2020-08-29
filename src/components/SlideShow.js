5441c9ea6040b6f5782a32f44e7f0f799f0b2f68
Orion-P <orionpoptean@gmail.com>
2020-08-29 12:18:55 -0700

    Added carousel for movie backdrops, title, date, and overview and incorporated that metadata into the home page ui.

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Slideshow.css";
 import axios from "axios";
+import Carousel from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+
 //56a76f413e126fc9841c4c59db456d01
 
-class SlideShow extends React.Component {
-	state = { movies: [], currentMovie: {} };
+const SlideShow = () => {
+	const [movies, setMovies] = useState([]);
+	const [currentMovie, setCurrentMovie] = useState({});
 
-	componentDidMount() {
+	useEffect(() => {
 		axios
 			.get("https://api.themoviedb.org/3/movie/upcoming", {
 				params: {
@@ -14,34 +18,42 @@ class SlideShow extends React.Component {
 				}
 			})
 			.then(res => {
-				this.setState({ movies: res.data.results });
-				this.setState({ currentMovie: this.state.movies[0] });
-				console.log(res.data.results);
+				setMovies(res.data.results);
+				setCurrentMovie(movies[0]);
 			})
 			.catch(err => {
 				console.log(err);
 			});
-	}
+	}, []);
 
-	render() {
+	console.log(movies, currentMovie);
+	const renderedMovies = movies.map(movie => {
 		return (
-			<div className="root">
-				<div className="slideshow">
-					<div className="ui text">
-						<h1 className="title">
-							{this.state.movies[0]
-								? this.state.movies[0].original_title
-								: null}
-						</h1>
-					</div>
+			<div style={{ width: "100%" }}>
+				<h1 className="title text">
+					{movie.original_title ? movie.original_title : null}
+				</h1>
+				<h2 className="date">{movie.release_date.slice(5) + "-" + movie.release_date.slice(0,4)}</h2>
+				<h2 className="overview">
+					{movie.overview.length < 100
+						? movie.overview
+						: movie.overview.slice(0, 100) + "..."}
+				</h2>
+				<div className="overlay">
 					<img
-						src={`https://image.tmdb.org/t/p/original${this.state.currentMovie.backdrop_path}`}
+						src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
 						className="image"
 					></img>
 				</div>
 			</div>
 		);
-	}
-}
+	});
+
+	return (
+		<div className="root">
+			<Carousel>{renderedMovies}</Carousel>
+		</div>
+	);
+};
 
 export default SlideShow;
